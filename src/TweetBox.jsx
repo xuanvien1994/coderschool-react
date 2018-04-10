38a854b499749ef0b00d46b0657fb5cd0377abfb
Vien Tran <vien.xuan.tran@steadfasttech.com.au>
2018-04-10 18:16:43 +0700

    initial commit create react project

---
+++ src/TweetBox.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class TweetBox extends Component {
+  render() {
+    return (
+     <h1>This is where Tweetbox Live </h1>
+    );
+  }
+}
+
+export default TweetBox;
